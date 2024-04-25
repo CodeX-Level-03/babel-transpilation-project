25bd3a448fa9a0bab42feb9217c16b795f747263
uxeeCoder <umar.afar@aol.com>
2024-04-25 01:22:29 -0400

    Refactor index.js to add basic arithmetic functions and user greeting

---
+++ src/index.js
@@ -0,0 +1,18 @@
+"use strict";
+
+
+
+const add = (a, b) => a + b;
+
+const divide = (a, b) => a / b;
+
+const userName = "Umar";
+console.log(`Hello, ${userName}`);
+
+
+
+let score = 0;
+score++;
+
+
+
